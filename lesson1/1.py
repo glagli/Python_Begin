e67aeacbf71c13235c5ccc12038badc842c263a8
Vladislav <Glagli@yandex.ru>
2021-03-31 18:12:35 +0300

    Добавление задач 1 урока в ветку

---
+++ lesson1/1.py
@@ -0,0 +1,26 @@
+# Поработайте с переменными, создайте несколько, выведите на экран, запросите у
+# пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+
+a = 1
+print(type(a), a)
+b = 'one'
+print(type(b), b)
+c = 1.1
+print(type(c), c)
+d = [1, 2, 3.3, 'abc']
+print(type(d), d)
+e = {1: 'one', 2: 'two'}
+print(type(e), e)
+f = True
+print(type(f), f)
+j = (1, 2, [1, 2], 'one')
+print(type(j), j)
+
+h = input('Введите любое слово: ')
+print(type(h), h)
+
+i = int(input('Введите любое целое число: '))
+print(type(i), i)
+
+g = float(input('Введите любое дробное число: '))
+print(type(g), g)
