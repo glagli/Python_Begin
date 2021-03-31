1309a0ad3cd64721358d94114dd030b976c48490
Vladislav <Glagli@yandex.ru>
2021-03-31 22:39:19 +0300

    Исправления

--- lesson1/4.py
+++ lesson1/4.py
@@ -1,7 +1,6 @@
 # Пользователь вводит целое положительное число. Найдите самую большую цифру в числе.
 # Для решения используйте цикл while и арифметические операции.
 number = int(input("Ведите число: "))
-<<<<<<< HEAD
 my_max = 0
 while number != 0:
     a = number % 10
@@ -9,11 +8,3 @@ while number != 0:
         my_max = a
     number = number // 10
 print('Cамая большая цифра в числе ', my_max)
-=======
-listik = []
-while number != 0:
-    a = number % 10
-    listik.append(a)
-    number = number // 10
-print('Cамая большая цифра в числе ', max(listik))
->>>>>>> lesson1
