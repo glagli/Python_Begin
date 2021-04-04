dce44fff1f72284147ee375deb636d72b7f57613
Vladislav <Glagli@yandex.ru>
2021-04-04 13:17:37 +0300

    Поправка во 2 задании

--- lesson1/2.py
+++ lesson1/2.py
@@ -2,18 +2,18 @@
 # выведите в формате чч:мм:сс. Используйте форматирование строк.
 
 time = int(input('Введите время в секундах: '))
-hour = int(time // 3600)
-minut = int((time - (hour * 3600)) // 60)
-second = int(time - (hour * 3600) - (minut * 60))
+hour = time // 3600
+minut = (time - (hour * 3600)) // 60
+second = (time - (hour * 3600)) - (minut * 60)
 
-if hour < 10:
-    hour = '0' + str(hour)
-if minut < 10:
-    minut = '0' + str(minut)
-if second < 10:
-    second = '0' + str(second)
+# if hour < 10:
+#     hour = '0' + str(hour)
+# if minut < 10:
+#     minut = '0' + str(minut)
+# if second < 10:
+#     second = '0' + str(second)
 
-print(f'{hour}:{minut}:{second}')
+print(f'{hour:02}:{minut:02}:{second:02}') # Хороший способ использование f формата
 
 # print('0'+str(hour) if hour < 10 else hour, end=':')
 # print('0'+str(minut) if minut < 10 else minut, end=':')
